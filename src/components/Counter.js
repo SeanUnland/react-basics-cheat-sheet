ecd5cd5e9b30b688d1168a2ab5501dfeab935d04
Sean Unland <sdunland@gmail.com>
2022-06-08 12:35:17 -0400

    Added another counter to my Counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 const Counter = ({}) => {
   const [count, setCount] = useState(0);
+  let [counterTwo, setCounterTwo] = useState(0)
 
   const handleAdd = () => {
     setCount(count + 1);
@@ -11,12 +12,35 @@ const Counter = ({}) => {
     setCount(count - 1);
   };
 
+  const addCounterTwo = () =>
+  {
+    setCounterTwo(counterTwo + 2)
+  }
+
+  const subtractCounterTwo = () =>
+  {
+    setCounterTwo(counterTwo - 2)
+  }
+
+  useEffect(() =>
+  {
+    if (setCounterTwo !== 0)
+    {
+      counterTwo = 0
+    }
+  }, [])
+
   return (
     <>
       <div>
         <button onClick={handleAdd}>Add</button>
         <p>{count}</p>
         <button onClick={handleSubtract}>Subtract</button>
+      </div>
+      <div>
+        <button onClick={addCounterTwo}>Add 2</button>
+        <h4>{counterTwo}</h4>
+        <button onClick={subtractCounterTwo}>Subtract 2</button>
       </div>
     </>
   );
